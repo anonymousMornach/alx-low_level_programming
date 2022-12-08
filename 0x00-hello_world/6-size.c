dd576b6fd846a975514e8fadee895b9fc8c34aa9
Taiwo Ifedayo <techmornach@gmail.com>
2022-12-08 13:31:47 +0000

    Push

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void){
 	printf("Size of a char : %i byte(s)", sizeof(char));
 	printf("Size of an int : %i byte(s)", sizeof(int));
