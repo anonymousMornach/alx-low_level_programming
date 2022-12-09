c6b094d6efdd68c745b00e34f97322594fe16a42
Taiwo Ifedayo <techmornach@gmail.com>
2022-12-09 15:07:20 +0000

    Push

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	char capital[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+		putchar(capital[i]);
+	}
+	putchar('\n');
+	return (0);
+}
