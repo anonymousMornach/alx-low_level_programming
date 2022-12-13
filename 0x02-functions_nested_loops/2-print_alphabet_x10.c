6bf4011568ef44f38d6e716b2c0fddc813351ccb
Taiwo Ifedayo <techmornach@gmail.com>
2022-12-13 20:41:40 +0000

    Push

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - Program prints alphabet in lowercase
+ * print_alphabet_x10 - Program prints alphabet in lowercase
  *
  * Always return 0 (Success)
  */
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 {
 	int i, j;
 
-	for(j = 0; j < 10 ; j++)
+	for (j = 0; j < 10 ; j++)
 	{
 		for (i = 97; i <= 122 ; i++)
 		{
