59439bcd7354f88d90cd1ecc9283f0898389442a
Taiwo Ifedayo <techmornach@gmail.com>
2022-12-13 20:48:24 +0000

    Push

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,12 @@
 #include "main.h"
 /**
- * _islower - Program prints alphabet in lowercase
+ * _islower - Shows 1 if the input is a
+ * lowercase character. Another cases, shows
+ * 0
  *
- * Always return 0 (Success)
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for lowercase character. 0 for the rest.
  */
 int _islower(int c)
 {
