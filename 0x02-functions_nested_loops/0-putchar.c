086b569f2d28d8f3c75f434ab39625b5acf95273
Taiwo Ifedayo <techmornach@gmail.com>
2022-12-13 18:42:21 +0000

    Push

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,12 @@
 int main(void)
 {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114, 10};
-	int i,al;
+	int i, al;
 
 	al = sizeof(str) / sizeof(int);
-	
-	for (i = 0 ;i < al ;i++)
+	for (i = 0 ; i < al ; i++)
 	{
 		_putchar(str[i]);
-	}	
+	}
 	return (0);
 }
