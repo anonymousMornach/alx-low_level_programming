a192d81e8401f927f17594b4382e58efe3a150b4
Taiwo Ifedayo <techmornach@gmail.com>
2022-12-14 02:50:58 +0000

    Push

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,16 +11,16 @@
 int print_last_digit(int n)
 {
 	int l;
-	
+
 	l = n % 10;
-	if (n < 0)
+	if (l < 0)
 	{
 		_putchar(-l + 48);
-		return (0);
+		return (-l);
 	}
 	else
 	{
 		_putchar(l + 48);
-		return (0);
+		return (l);
 	}
 }
