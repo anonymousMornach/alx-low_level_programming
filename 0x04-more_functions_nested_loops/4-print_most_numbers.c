975f2da0b3c4e80b9df4279b81dd4aaf499d318c
Taiwo Ifedayo <techmornach@gmail.com>
2022-12-15 14:26:13 +0000

    Push

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,9 +8,10 @@ void print_most_numbers(void)
 {
 	int i;
 	
-	for (i = 48; ((i < 58) && (i != 50 || i != 52)); i++)
+	for (i = 48; i < 58; i++)
 	{
-		_putchar(i);
+		if (i != 50 && i != 52)
+			_putchar(i);
 	}
 	_putchar(10);
 }
