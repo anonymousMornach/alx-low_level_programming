7b910125e8a3b156f70980f480515c80e45adea5
Taiwo Ifedayo <techmornach@gmail.com>
2022-12-15 16:27:13 +0000

    Push

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Program prints \ then a new line
+ *
+ * @n: Character input
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n);
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
