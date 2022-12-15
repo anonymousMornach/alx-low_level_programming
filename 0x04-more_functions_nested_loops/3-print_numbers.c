ba8bb0c3fe8b8314f9bc2760a1e39a8b04a9be7e
Taiwo Ifedayo <techmornach@gmail.com>
2022-12-15 14:14:16 +0000

    Push

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints 0 - 9 followed by a newline
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+}
