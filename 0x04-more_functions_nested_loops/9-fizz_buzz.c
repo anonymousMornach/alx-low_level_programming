bfbdfd1dc81a27eed7ffe3b60612a6f660090a42
Taiwo Ifedayo <techmornach@gmail.com>
2022-12-15 20:39:20 +0000

    Push

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,24 +7,26 @@
 int main(void)
 {
 	int i;
-
-	for (i = 1; i <= 100; i++)
+	
+	i = 0;
+	printf("%d", i);
+	for (i = 2; i <= 100; i++)
 	{
 		if ((i % 15) == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if ((i % 3) == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if ((i % 5) == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else
 		{
-			printf("%i ", i);
+			printf(" %d", i);
 		}
 	}
 	printf("\n");
