6cb3070c41c8e0174c26685711f42700987fac07
Taiwo Ifedayo <techmornach@gmail.com>
2022-12-19 20:31:41 +0000

    Push

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - program accept a parameter and updates the value
- * @*n: Integer input
+ * @n: Integer input
  * Return: void
  */
 void reset_to_98(int *n)
