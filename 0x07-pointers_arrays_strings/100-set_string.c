dc016a0c574f65e359daad2c26915ef79e037c20
Taiwo Ifedayo <techmornach@gmail.com>
2023-01-03 04:15:11 +0000

    Push

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,18 @@
+#include "main"
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be reversed
+ */
+
+void print_rev(char *s)
+{
+	int len, i;
+
+	len = 0;
+	i = 0;
+	while (s[i++])
+		lens++;
+	for (i = len-1; i>=0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
